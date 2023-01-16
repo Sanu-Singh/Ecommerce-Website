5dcaefd63dbc72a170cbf7362fc3bdbd46eed117
Sanu Singh Rajput <81958742+Sanu-Singh@users.noreply.github.com>
2023-01-17 03:07:42 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Categorieslist from './components/Categorieslist/Categorieslist';
+
 function App() {
   return (
     <div >
--- src/components/Categorieslist/Categorieslist.js
+++ src/components/Categorieslist/Categorieslist.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Categorieslist.css";
-
+import Card from "./Card";
 // Top offers:https://rukminim1.flixcart.com/fk-p-flap/128/128/image/47e5c31ef7dcce1e.png?q=100
 // Mobiles & Tablets: https://rukminim1.flixcart.com/fk-p-flap/128/128/image/5f8bd5669e19e724.png?q=100
 // Electronics: https://rukminim1.flixcart.com/fk-p-flap/128/128/image/361d53b8725c2d2d.png?q=100
@@ -11,46 +11,68 @@ import "./Categorieslist.css";
 // Flights: https://rukminim1.flixcart.com/fk-p-flap/128/128/image/007910082ae6355b.png?q=100
 // Grocery:https://rukminim1.flixcart.com/fk-p-flap/128/128/image/46376ceed3448aff.png?q=100
 
-
+const categoriesData = [
+   {
+    id:1,
+    image:"https://rukminim1.flixcart.com/fk-p-flap/128/128/image/47e5c31ef7dcce1e.png?q=100",
+    text:"Top Offers",
+   
+},
+{
+    id: 2,
+    image:
+      "https://rukminim1.flixcart.com/fk-p-flap/128/128/image/5f8bd5669e19e724.png?q=100",
+    text: "Mobiles",
+  },
+  {
+    id: 3,
+    image:
+      "https://rukminim1.flixcart.com/fk-p-flap/128/128/image/361d53b8725c2d2d.png?q=100",
+    text: "Electronics",
+  },
+  {
+    id: 4,
+    image:
+      "https://rukminim1.flixcart.com/fk-p-flap/128/128/image/904f3c8e7101408b.png?q=100",
+    text: "TVs",
+  },
+  {
+    id: 5,
+    image:
+      "https://rukminim1.flixcart.com/fk-p-flap/128/128/image/7f7355480c6adc16.png?q=100",
+    text: "Fashion",
+  },
+  {
+    id: 6,
+    image:
+      "https://rukminim1.flixcart.com/fk-p-flap/128/128/image/f97d6138516056bc.png?q=100",
+    text: "Beauty",
+  },
+  {
+    id: 7,
+    image:
+      "https://rukminim1.flixcart.com/fk-p-flap/128/128/image/6728ed3cf145562c.png?q=100",
+    text: "Furniture",
+  },
+  {
+    id: 8,
+    image:
+      "https://rukminim1.flixcart.com/fk-p-flap/128/128/image/007910082ae6355b.png?q=100",
+    text: "Flights",
+  },
+  {
+    id: 9,
+    image:
+      "https://rukminim1.flixcart.com/fk-p-flap/128/128/image/46376ceed3448aff.png?q=100",
+    text: "Grocery",
+  },
+  ];
 function Categorieslist() {
     return (
         <div className="Category_list">
-        <div className="Single_category">
-           <img src="https://rukminim1.flixcart.com/fk-p-flap/128/128/image/47e5c31ef7dcce1e.png?q=100"/> 
-           <p>Top Offers</p>
-        </div>
-        <div className="Single_category">
-           <img src="https://rukminim1.flixcart.com/fk-p-flap/128/128/image/5f8bd5669e19e724.png?q=100"/> 
-           <p>Mobiles & Tablets</p>
-        </div>
-        <div className="Single_category">
-           <img src="https://rukminim1.flixcart.com/fk-p-flap/128/128/image/361d53b8725c2d2d.png?q=100"/> 
-           <p>Electronics Appliances</p>
-        </div>
-        <div className="Single_category">
-           <img src="https://rukminim1.flixcart.com/fk-p-flap/128/128/image/904f3c8e7101408b.png?q=100"/> 
-           <p>TVs & Appliances</p>
-        </div>
-        <div className="Single_category">
-           <img src="https://rukminim1.flixcart.com/fk-p-flap/128/128/image/7f7355480c6adc16.png?q=100"/> 
-           <p>Fashion</p>
-        </div>
-        <div className="Single_category">
-           <img src="https://rukminim1.flixcart.com/fk-p-flap/128/128/image/f97d6138516056bc.png?q=100"/> 
-           <p>Beauty</p>
-        </div>
-        <div className="Single_category">
-           <img src="https://rukminim1.flixcart.com/fk-p-flap/128/128/image/6728ed3cf145562c.png?q=100"/> 
-           <p>Home Furnitures</p>
-        </div>
-        <div className="Single_category">
-           <img src="https://rukminim1.flixcart.com/fk-p-flap/128/128/image/007910082ae6355b.png?q=100"/> 
-           <p>Flights</p>
-        </div>
-        <div className="Single_category">
-           <img src="https://rukminim1.flixcart.com/fk-p-flap/128/128/image/46376ceed3448aff.png?q=100"/> 
-           <p>Grocery</p>
-        </div>
+        {categoriesData.map((value) => (
+            <Card image={value.image} text={value.text} />
+      ))}
         </div>
     );
 }
