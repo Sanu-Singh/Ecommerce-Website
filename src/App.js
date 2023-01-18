ecf4e652960ee5b7bda471d2155e04fdb6cc6492
Sanu Singh Rajput <81958742+Sanu-Singh@users.noreply.github.com>
2023-01-19 00:26:16 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Categorieslist from './components/Categorieslist/Categorieslist';
-
+import Carousel from "./components/Carousel/Carousel";
 function App() {
   return (
     <div >
      <Navbar/>
     <Categorieslist/>
+    <Carousel/>
     </div>
   );
 };
